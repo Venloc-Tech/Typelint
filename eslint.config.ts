b5f05a810482ff8c2b552f642987464150bf6a83
0x303133 <132019318+shiz-ceo@users.noreply.github.com>
2025-03-30 18:18:02 +0300

    chore: Configure internal config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,14 @@
 import tseslint from "typescript-eslint";
-import { VenlocRecommended } from "./src/index.js";
+import { join } from "path";
 
-export default tseslint.config(VenlocRecommended);
+import { allTypescriptFiles, setIgnoreFiles, setParserOptions, VenlocRecommended } from "./src/index.js";
+
+
+const root = process.cwd();
+
+export default tseslint.config({
+  extends: VenlocRecommended,
+  files: allTypescriptFiles,
+  ...setParserOptions({ project: "./tsconfig.eslint.json" }),
+  ...setIgnoreFiles(join(root, ".gitignore")),
+});
